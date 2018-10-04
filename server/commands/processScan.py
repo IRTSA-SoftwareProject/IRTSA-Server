97982b979e638918e602244549b3b8e24aaf6d9c
jpmoran-2053942 <2053942@student.swin.edu.au>
2018-10-04 15:48:54 +1000

    Added composite function

--- server/commands/processScan.py
+++ server/commands/processScan.py
@@ -1,8 +1,6 @@
 from server.messages import message
 from server.events import events, of_type, via_asyncio
-from server.commands.ris_processing import read_ris
-from server.commands.ris_processing import file_io_thermal
-from server.commands.ris_processing import process_image
+from server.commands.ris_processing import image_process_interface
 import numpy
 import datetime
 import base64
@@ -12,26 +10,7 @@ async def processScan(event):
     print('Running scan')
     #get current time to save scan
     time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    thermogram = file_io_thermal.open_png('/home/pi/scans/png/')
-    await connection.send(message('scan_progress', {'percent': 30}))
-    print('Saving thermogram to .gif...')
-    if not file_io_thermal.save_gif(thermogram, '/var/www/html/irscans/' + time + '.gif'):
-        print('Failed to save .gif :(')
-    await connection.send(message('scan_progress', {'percent': 60}))
-    print('Processing image...')
-    phasemap = process_image.process_image(thermogram, frame_length = -1, xStartSkip = 0, xEndSkip = 0, yStartSkip = 0)
-    await connection.send(message('scan_progress', {'percent': 95}))
-    print('Saving phasemap to .png...')
-    if phasemap.shape[0] == 1: #Check if there is only one thermogram
-        if not file_io_thermal.save_png(phasemap, '/var/www/html/irscans/' + time + '.png'):
-            print('Failed to save .png :(')
-    else: #Save all thermograms if more than one was produced
-        for i in range(0, phasemap.shape[0] - 1):
-            if not file_io_thermal.save_png(phasemap[i,:,:], '/var/www/html/irscans/' + time + '{0:04d}'.format(i) + '.png'):
-                print('Failed to save .png :(')
-    scanImg = open('/var/www/html/irscans/' + time + '.png', 'rb')
-    scan = base64.b64encode(scanImg.read())
-    scanImg.close()
+    ris_processing.image_process_interface.process_image(connection.send, 'C:/PNG Dumps/Processing/'+time, 'Final Thermal Hotspot', 0, -1)
     await connection.send(message('scan_progress', {'percent': 100}))
     await connection.send(message('scan_complete', {'base64EncodedString': scan.decode('utf-8')}))
     print('Scan complete')
--- src/demo/pngProcessingDemo.py
+++ src/demo/pngProcessingDemo.py
@@ -2,34 +2,16 @@
 # Example of using the ris_processing package
 # @author: James Moran edited by Casper Pikaar
 
-import ris_processing.read_ris
-import ris_processing.file_io_thermal
-import ris_processing.process_image
-import numpy
 import sys
+import asyncio
+import ris_processing.image_process_interface
 
+async def printforme(stringtoPrint):
+    print(stringtoPrint)
+    
 #Time variable which has time passed to it from command executing script
 time = sys.argv[1]
 
 if __name__ == '__main__':
-    print('Reading file...')
-    thermogram = ris_processing.file_io_thermal.open_png('C:/PNG Dumps/1cm-s Greyscale/0')
+    ris_processing.image_process_interface.process_image(printforme, 'C:/PNG Dumps/Processing/'+time, 'Final Thermal Hotspot', 0, -1)
     
-    print('Saving thermogram to .gif...')
-    if not ris_processing.file_io_thermal.save_gif(thermogram, 'C:/PNG Dumps/Processing/out.gif'):
-        print('Failed to save .gif :(')
-        
-    print('Processing image...')
-    #phasemap = ris_processing.process_image.process_image(thermogram, frame_length = 10, xStartSkip = 45, xEndSkip = 60, yStartSkip = 175)
-    phasemap = ris_processing.process_image.process_image(thermogram, frame_length = -1, xStartSkip = 0, xEndSkip = 0, yStartSkip = 0)
-    
-    print('Saving phasemap to .png...')
-    if phasemap.shape[0] == 1: #Check if there is only one thermogram
-        if not ris_processing.file_io_thermal.save_png(phasemap, 'C:/PNG Dumps/Processing/out.png'):
-            print('Failed to save .png :(')
-    else: #Save all thermograms if more than one was produced
-        for i in range(0, phasemap.shape[0] - 1):
-            if not ris_processing.file_io_thermal.save_png(phasemap[i,:,:], 'C:/PNG Dumps/Processing/out-' + '{0:04d}'.format(i) + '.png'):
-                print('Failed to save .png :(')
-    
-    print("Done! Process Complete.")
