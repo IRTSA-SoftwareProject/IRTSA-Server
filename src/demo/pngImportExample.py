2a43435ff597f3aaa7fa92701e19def8553710f3
jpmoran-2053942 <2053942@student.swin.edu.au>
2018-10-09 22:01:11 +1100

    added TSR and image subtraction processing methods

--- src/demo/pngImportExample.py
+++ src/demo/pngImportExample.py
@@ -13,34 +13,23 @@ time = sys.argv[1]
 
 if __name__ == '__main__':
     print('Reading file...')
-    thermogram = ris_processing.file_io_thermal.open_png('/var/www/html/irscans/PNG' + time)
+    thermogram = ris_processing.file_io_thermal.open_png('C:/PNG Dumps/1cm-s Greyscale/0')
     
-    print('Saving thermogram to .gif...')
-    if not ris_processing.file_io_thermal.save_gif(thermogram, '/var/www/html/irscans/' + time + '.gif'):
-        print('Failed to save .gif :(')
-    
-    '''
-    print('Saving thermogram to PNG*-X.png...')
-    if thermogram.shape[0] == 1: #Check if there is only one thermogram
-        if not ris_processing.file_io_thermal.save_png(thermogram, '/var/www/html/irscans/' + time + '.png'):
-            print('Failed to save .png :(')
-    else: #Save all thermograms if more than one was produced
-        for i in range(0, thermogram.shape[0]):
-            if not ris_processing.file_io_thermal.save_png(thermogram[i,:,:], '/var/www/html/irscans/PNG' + time + '-' + '{0:04d}'.format(i) + '.png'):
-                print('Failed to save .png :(')
-    '''
-    
+    #print('Saving thermogram to .gif...')
+    #if not ris_processing.file_io_thermal.save_gif(thermogram, 'C:/PNG Dumps/Processing/out.gif'):
+    #    print('Failed to save .gif :(')
+        
     print('Processing image...')
     #phasemap = ris_processing.process_image.process_image(thermogram, frame_length = 10, xStartSkip = 45, xEndSkip = 60, yStartSkip = 175)
-    phasemap = ris_processing.process_image.process_image(thermogram, frame_length = -1, xStartSkip = 0, xEndSkip = 0, yStartSkip = 0)
+    phasemap = ris_processing.process_image.process_image(thermogram[1:20, :, :], frame_length = -1, xStartSkip = 0, xEndSkip = 0, yStartSkip = 0, yEndSkip = 0)
     
     print('Saving phasemap to .png...')
     if phasemap.shape[0] == 1: #Check if there is only one thermogram
-        if not ris_processing.file_io_thermal.save_png(phasemap, '/var/www/html/irscans/' + time + '.png'):
+        if not ris_processing.file_io_thermal.save_png(phasemap, 'C:/PNG Dumps/Processing/out.png'):
             print('Failed to save .png :(')
     else: #Save all thermograms if more than one was produced
         for i in range(0, phasemap.shape[0] - 1):
-            if not ris_processing.file_io_thermal.save_png(phasemap[i,:,:], '/var/www/html/irscans/' + time + '-' + '{0:04d}'.format(i) + '.png'):
+            if not ris_processing.file_io_thermal.save_png(phasemap[i,:,:], 'C:/PNG Dumps/Processing/out-' + '{0:04d}'.format(i) + '.png'):
                 print('Failed to save .png :(')
     
     print("Done! Process Complete.")
